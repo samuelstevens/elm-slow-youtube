4470e6d900a18155735b8fb984a0f27abf4082ff
Sam Stevens <stevens.994@osu.edu>
2020-08-08 10:50:23 -0400

    better build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+mkdir -p build
+cp src/index.html build/
+sed -i '' 's/debug/build/' build/index.html # changes main.debug.js to main.build.js
+cp src/main.css build/
+
 elm make src/Main.elm --optimize --output=build/main.build.js
--- dev.sh
+++ dev.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-serve dev &
+build() {
+  mkdir -p debug
+  cp src/index.html debug/
+  cp src/main.css debug/
+  elm make src/Main.elm --debug --output=debug/main.debug.js
+}
+
+export -f build
+
+serve debug &
 
 sleep 1
 
 open "$(pbpaste)"
 
-find src | entr elm make src/Main.elm --debug --output=dev/main.debug.js
+find src | entr -s build
